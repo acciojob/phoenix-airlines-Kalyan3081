0a1a58720ea2d238964f203205f6306b3198e133
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-12-18 19:58:17 +0530

    Update Landingpage.jsx

--- src/components/Landingpage.jsx
+++ src/components/Landingpage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import styles from './PhoenixAirlines.css'
-import { Link } from 'react-router'
+import './PhoenixAirlines.css'
+    import { Link } from 'react-router'
 
 const Landingpage = () => {
     return (

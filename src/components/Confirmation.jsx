87f7e4776db1c1940226ff5aa70e5737b3cd6cce
Kalyan3081 <162336115+Kalyan3081@users.noreply.github.com>
2024-12-18 19:48:18 +0530

    Update Confirmation.jsx

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import styles from './PhoenixAirlines.module.css'
+import styles from './PhoenixAirlines.css'
 import { Link } from 'react-router'
 
 
 const Confirmation = () => {
     return (
         <>
-            <nav className={styles.navBar}>
+            <nav className='navBar'>
                 <h3>Flight Booking App</h3>
             </nav>
             <p>Thank you for the booking. Click the below button to return to home page</p>
             <Link to="/">
-                <button className={styles.Landingpagebtn}>back to home</button>
+                <button className='Landingpagebtn'>back to home</button>
             </Link>
         </>
     )
